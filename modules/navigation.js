e59f620398053acfec6adfb4ebb57a0f4f3aab1e
Lisa Søndergaard <lisa3074@stud.kea.dk>
2020-05-14 10:12:07 +0200

    custum properties set up

---
+++ modules/navigation.js
@@ -0,0 +1,6 @@
+export function delegation() {
+  console.log("delegation");
+}
+export function start() {
+  console.log("start");
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+import "@babel/polyfill";
+import { delegation } from "./modules/navigation";
+import { start as navStart } from "./modules/navigation";
