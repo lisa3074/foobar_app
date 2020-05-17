e29607191fe4c93f2074874f981ed22a7fd2c86c
Lisa Søndergaard <lisa3074@stud.kea.dk>
2020-05-18 00:11:41 +0200

    styling of index (except from winner), and queue working, menu working and all animations for these

--- modules/queue.js
+++ modules/queue.js
@@ -1,23 +1,106 @@
+const url = "https://foobar3rdsemester.herokuapp.com/";
+const array = [];
+let jsonData;
+let beforeLastServed;
+let oldBeerCount = 0;
+//let beerCount;
+
+const Queue = {
+  queueNumber: "",
+  loggedAt: "",
+  servedToday: "",
+};
+
 export function indexDelegation() {
   console.log("indexDelegation");
   //SET EVENTLISTNERE TIL DE STORE IKONER
   loadJson();
 }
 
-function loadJson() {
+async function loadJson() {
   console.log("loadJson");
+  let response = await fetch(url);
+  jsonData = await response.json();
   makeObjects();
 }
 function makeObjects() {
   console.log("makeObjects");
-  loops();
+  const foobarObject = Object.create(Queue);
+  console.log(jsonData);
+  foobarObject.queueNumber = jsonData.queue.length;
+  foobarObject.serving1 = JSON.parse(jsonData.bartenders[0]["servingCustomer"]);
+  foobarObject.serving2 = JSON.parse(jsonData.bartenders[1]["servingCustomer"]);
+  foobarObject.serving3 = JSON.parse(jsonData.bartenders[2]["servingCustomer"]);
+  foobarObject.servedToday = Math.max(foobarObject.serving1, foobarObject.serving2, foobarObject.serving3);
+  if (foobarObject.queueNumber > 0) {
+    foobarObject.orderNumber = JSON.parse(jsonData.queue[0]["id"]);
+    foobarObject.loggedAt = JSON.parse(jsonData.queue[0]["startTime"]);
+
+    //checks how many beer were ordered in each order to get the full number of ordered beers (only works until reload)
+    for (let i = 0; i < foobarObject.queueNumber; i++) {
+      foobarObject.order = jsonData.queue[i].order.length;
+      oldBeerCount += foobarObject.order;
+      console.log(oldBeerCount);
+    }
+  }
+  foobarObject.beerCount = oldBeerCount;
+
+  //foobarObject.beingPoured = jsonData.bartenders.servingCustomer;
+  console.log(foobarObject);
+  loops(foobarObject);
 }
-function loops() {
+function loops(foobarObject) {
   console.log("loops");
-  displayQueue();
+  const queue = foobarObject.queueNumber;
+  array.unshift(queue);
+  if (array.length <= 5) {
+    console.log("under 10");
+  } else {
+    console.log("over 10");
+    array.pop();
+  }
+  //console.log(array);
+
+  displayQueue(foobarObject);
 }
-function displayQueue() {
-  console.log("displayQueue");
-  //der skal en setTimeOut på inden den sættes til, så det ikke looper
-  //loadJson();
+function displayQueue(foobarObject) {
+  document.querySelector(".wrap:nth-child(1) > .q_bar").classList.remove("fullHeight");
+  for (let number = 0; number < 5; number++) {
+    console.log("displayQueue");
+    const timeStamp = document.querySelector(".time");
+    const queueNow = document.querySelector(".queue_num");
+    const served = document.querySelector(".served_today");
+    const wins = document.querySelector(".wins_today");
+
+    //Convert time from milliseconds
+    const time = new Date().getTime();
+    const date = new Date(time);
+    let rightTime = date.toString();
+    //Make sure I only get the time
+    const theRightIime = rightTime.substring(15, 21);
+
+    //The first bar (for animation)
+    document.querySelector(".wrap:nth-child(1) > .q_bar").classList.add("nullHeight");
+    //The other bars
+    const bar = document.querySelector(`.queue_box > .wrap:nth-child(${number + 1}) > .q_bar`);
+    const barNum = document.querySelector(`.queue_box > .wrap:nth-child(${number + 1}) > .q_num`);
+    bar.style.setProperty("--height", array[number]);
+
+    //console.log(array[number]);
+    let winsNow = Math.floor(foobarObject.servedToday / 100);
+    if (foobarObject.servedToday == 0) {
+      foobarObject.servedToday = beforeLastServed;
+    }
+    timeStamp.textContent = theRightIime;
+    queueNow.textContent = foobarObject.queueNumber;
+    barNum.textContent = array[number];
+    wins.textContent = winsNow;
+    served.textContent = foobarObject.servedToday;
+    beforeLastServed = foobarObject.servedToday;
+  }
+  setTimeout(() => {
+    loadJson();
+    document.querySelector(".wrap:nth-child(1) > .q_bar").classList.remove("nullHeight");
+    document.querySelector(".wrap:nth-child(1) > .q_bar").classList.add("fullHeight");
+  }, 10000);
 }
--- script.js
+++ script.js
@@ -5,6 +5,7 @@ import { cartDelegation } from "./modules/order";
 import { toggleMenu } from "./modules/navigation";
 import { indexDelegation } from "./modules/queue";
 import { getData } from "./modules/win";
+import { chosen } from "./modules/navigation";
 import { loadJson as bigQueue } from "./modules/bigQueue";
 import { getData as bigWin } from "./modules/bigWin";
 import { loadJson as bigLine } from "./modules/bigLine";
@@ -15,23 +16,37 @@ function appDelegation() {
   console.log("appDelegation");
   const loginBody = document.querySelector(".loginBody");
   const orderBody = document.querySelector(".orderBody");
+  const beerBody = document.querySelector(".beerBody");
   const indexBody = document.querySelector(".indexBody");
   const winBody = document.querySelector(".winBody");
   const dashBody = document.querySelector(".dashBody");
 
   if (loginBody) {
     document.querySelector(".grid_item3 .log_into_account").addEventListener("click", checkValidity);
+    document.querySelector(".menu-login").classList.add("chosen");
+    document.querySelector(".menu-login .icon_wrapper").classList.add("chosen_img");
     toggleMenu();
   }
   if (orderBody) {
+    document.querySelector(".menu-buy").classList.add("chosen");
+    document.querySelector(".menu-buy .icon_wrapper").classList.add("chosen_img");
     cartDelegation();
     toggleMenu();
   }
+  if (beerBody) {
+    document.querySelector(".menu-beer").classList.add("chosen");
+    document.querySelector(".menu-beer .icon_wrapper").classList.add("chosen_img");
+    toggleMenu();
+  }
   if (indexBody) {
+    document.querySelector(".menu-home").classList.add("chosen");
+    document.querySelector(".menu-home .icon_wrapper").classList.add("chosen_img");
     indexDelegation();
     toggleMenu();
   }
   if (winBody) {
+    document.querySelector(".menu-win").classList.add("chosen");
+    document.querySelector(".menu-win .icon_wrapper").classList.add("chosen_img");
     getData();
     toggleMenu();
   }
